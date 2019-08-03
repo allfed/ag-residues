62e8d5a3ed83c827a9fddabde8c5aedf98b9861c
Tim Fist <timf@dasintel.io>
2019-08-03 15:07:17 +1000

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Code for data processing and analysis of agricultural residues as an alternate food source in catastrophic scenarios',
+    author='ALLFED',
+    license='MIT',
+)
